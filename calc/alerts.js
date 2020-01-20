a74277869cf79cf4543e6ecc6ac10efc92987976
brett.marshall@shaw.ca <brett.marshall@shaw.ca>
2020-01-19 19:54:38 -0500

    Lab1

---
+++ calc/alerts.js
@@ -0,0 +1,21 @@
+  function equal() {
+    alert("=");
+  }
+  function plus() {
+    alert("+");
+  }
+  function divide() {
+    alert("/");
+  }
+  function multiply() {
+    alert("x");
+  }
+  function subtract() {
+    alert("-");
+  }
+  function clear() {
+    alert("clear");
+  }
+  function number(){
+      alert("number");
+  }
