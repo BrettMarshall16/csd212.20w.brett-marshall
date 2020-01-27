fa7c8ddedf6f9c4a7eb11da649dae93a5782b8ad
brett.marshall@shaw.ca <brett.marshall@shaw.ca>
2020-01-26 22:08:06 -0500

    final

--- calc/alerts.js
+++ calc/alerts.js
@@ -1,42 +1,6 @@
-let display = document.getElementById('display')
-
-  function equal() {
-    alert("=");
-  }
-  function plus() {
-    alert("+");
-  }
-  function divide() {
-    alert("/");
-  }
-  function multiply() {
-    alert("x");
-  }
-  function subtract() {
-    alert("-");
-  }
-
-  function buttonClick(theButton){
-    let pushed = theButton.innterHTML;
-    if (pushed == 'clear'){
-      display.innerHTML = '0';
-    }
-    else{
-      display.innerHTML += pushed;
-    }
-  }
- /* function buttonClick(theButton){
-    let pushed = theButton.innerHTML
-    
-    if (pushed == "clear"){
-      display.innerHTML = "0"
-    }
-    else{
-      if(display.innerHTML = "0"){
-        display.innerHTML = pushed;
-      }
-      else {
-        display.innerHTML += pushed;
-      }
-    }
-  }*/
+function insert(num){
+  document.form.display.value = document.form.display.value+num;
+}
+function clean(){
+  document.form.display.value = "0";
+}
